1aa649af90fba995a53b849ab87443624ec8a51c
Aaron <trapp.root@gmail.com>
2024-08-01 19:07:39 -0700

    JS logic refined. Need to correctly coerce data type to number

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector("input")
+
+buttons.forEach((button) => {
+	const buttonClass = button.className;
+	switch (buttonClass) {
+		case "number":
+			button.addEventListener("click", handleDigits);
+			break;
+		case "operator":
+			button.addEventListener("click", handleOperators);
+			break;
+		case "equals":
+			button.addEventListener("click", handleEquals);
+			break;
+		case "decimal":
+			button.addEventListener("click", handleDecimal);
+			break;
+	}
+});
+
 let firstInteger;
 let secondInteger;
 let operator;
+let result;
 
 const add = (a, b) => {
 	return a + b;
@@ -16,18 +38,38 @@ const divide = (a, b) => {
 };
 
 const operate = (a, b, operator) => {
+    console.log(a, b, operator)
 	switch (operator) {
-		case "+":
-			add(a, b);
+		case "add":
+			results.value = add(a, b);
 			break;
-		case "-":
-			subtract(a, b);
+		case "subtract":
+			results.value = subtract(a, b);
 			break;
-		case "*":
-			multiply(a, b);
+		case "multiply":
+			results.value = multiply(a, b);
 			break;
-		case "/":
-			divide(a, b);
-            break;
+		case "divide":
+			results.value = divide(a, b);
+			break;
 	}
 };
+
+function handleDigits(e) {
+	firstInteger = e.target.id;
+    secondInteger = 42
+}
+
+function handleOperators(e) {
+	operator = e.target.id
+}
+
+function handleDecimal() {
+	
+}
+
+function handleEquals() {
+	if (firstInteger && secondInteger && operator) {
+        operate(firstInteger, secondInteger, operator)
+    }
+}
